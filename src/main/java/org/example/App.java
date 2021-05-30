aeb7b14262ac0c1c5df30726d1a3bbfda0f967fb
Gabriel Desktop <gabrielfernandez@knights.ucf.edu>
2021-05-30 18:06:31 -0400

    Updated spacing

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -22,36 +22,37 @@ public class App
 
         // Scan in the values for the prices and quantities of each
         // item and store into a double datatype
-        System.out.println("Enter the price of item 1: ");
+        System.out.println("Enter the price of item 1:");
         Scanner item1PIn = new Scanner(System.in);
         double item1P = item1PIn.nextInt();
 
-        System.out.println("Enter the quantity of item 1: ");
+        System.out.println("Enter the quantity of item 1:");
         Scanner item1QIn = new Scanner(System.in);
         double item1Q = item1QIn.nextInt();
 
-        System.out.println("Enter the price of item 2: ");
+        System.out.println("Enter the price of item 2:");
         Scanner item2PIn = new Scanner(System.in);
         double item2P = item2PIn.nextInt();
 
-        System.out.println("Enter the quantity of item 2: ");
+        System.out.println("Enter the quantity of item 2:");
         Scanner item2QIn = new Scanner(System.in);
         double item2Q = item2QIn.nextInt();
 
-        System.out.println("Enter the price of item 3: ");
+        System.out.println("Enter the price of item 3:");
         Scanner item3PIn = new Scanner(System.in);
         double item3P = item3PIn.nextInt();
 
-        System.out.println("Enter the quantity of item 3: ");
+        System.out.println("Enter the quantity of item 3:");
         Scanner item3QIn = new Scanner(System.in);
         double item3Q = item3QIn.nextInt();
 
         // Calculate the subtotal, tax, and total
-        double subtotal = (item1P * item1Q) + (item2P * item2Q) + (item3P * item3Q);
+        double subtotal = (item1P * item1Q) + (item2P * item2Q)
+                        + (item3P * item3Q);
         double tax = subtotal * TAX;
         double total = subtotal + tax;
 
-        // Round all values to 2 decimal places
+        // Round to 2 decimal places
         DecimalFormat df = new DecimalFormat("0.00");
         df.setRoundingMode(RoundingMode.CEILING);
 
